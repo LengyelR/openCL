7dd2e8dc212ff82a05c6a966d6fdde381cc3f647
LengyelR <richard.lengyel@yahoo.com>
2015-02-18 08:10:08 +0100

    Update vector_addition.h

--- vector_addition.h
+++ vector_addition.h
@@ -24,7 +24,7 @@ class VectorAddition
         cl_mem bufC;
         
         // CL objects
-		cl_int status;
+	cl_int status;
         cl_uint numPlatforms;
         cl_uint numDevices;
         cl_platform_id *platforms;
@@ -32,79 +32,78 @@ class VectorAddition
         cl_context context;
         cl_command_queue cmdQueue;
         cl_program program;
-		cl_kernel kernel;
+	cl_kernel kernel;
 
-		//Info Objects(platform)
-		char *profile;
-		char *platform_version;
+	//Info Objects(platform)
+	char *profile;
+	char *platform_version;
 
-		//Info Objects(device)
-		char *vendor;					//CL_DEVICE_VENDOR
-		char *device_name;			 //CL_DEVICE_NAME
-		char *open_cl_c_version;			//CL_DEVICE_OPENCL_C_VERSION  ---- openCL C
-		char *open_cl_version;			// CL_DEVICE_VERSION --- openCL
-		size_t *max_workgroup;		// CL_DEVICE_MAX_WORK_GROUP_SIZE
-		cl_uint *max_cu;					//CL_DEVICE_MAX_COMPUTE_UNITS
-		cl_uint *max_freq;				//CL_DEVICE_MAX_CLOCK_FREQUENCY
+	//Info Objects(device)
+	char *vendor;				// CL_DEVICE_VENDOR
+	char *device_name;			// CL_DEVICE_NAME
+	char *open_cl_c_version;		// CL_DEVICE_OPENCL_C_VERSION
+	char *open_cl_version;			// CL_DEVICE_VERSION
+	size_t *max_workgroup;			// CL_DEVICE_MAX_WORK_GROUP_SIZE
+	cl_uint *max_cu;			// CL_DEVICE_MAX_COMPUTE_UNITS
+	cl_uint *max_freq;			// CL_DEVICE_MAX_CLOCK_FREQUENCY
         
     public:
         VectorAddition()
               : elements(NUM_OF_ELEMENTS),
                 datasize(sizeof(int)*elements),
-				A((int*)malloc(datasize)), //ezekek szintén?
-				B((int*)malloc(datasize)),
-				C((int*)malloc(datasize)),
-				numPlatforms(0),
-				platforms(NULL), // ez jo-e igy?
-				numDevices(0),
+		A((int*)malloc(datasize)), //ezekek szintÃ©n?
+		B((int*)malloc(datasize)),
+		C((int*)malloc(datasize)),
+		numPlatforms(0),
+		platforms(NULL), // ez jo-e igy?
+		numDevices(0),
 
-				//PlatformInfo
-				profile(NULL),
-				platform_version(NULL),
+		//PlatformInfo
+		profile(NULL),
+		platform_version(NULL),
 
-				//DeviceInfo
-				devices(NULL),
-				vendor(NULL),
-				device_name(NULL),
-				open_cl_c_version(NULL),
-				open_cl_version(NULL),
-				max_workgroup(NULL),
-				max_cu(NULL),
-				max_freq(NULL)
-
+		//DeviceInfo
+		devices(NULL),
+		vendor(NULL),
+		device_name(NULL),
+		open_cl_c_version(NULL),
+		open_cl_version(NULL),
+		max_workgroup(NULL),
+		max_cu(NULL),
+		max_freq(NULL)
         {
-			for(int i=0; i<elements; i++) {
-				A[i]=i;
-				B[i]=i;
-			}
+		for(int i=0; i<elements; i++) {
+			A[i]=i;
+			B[i]=i;
+		}
 
         }
             
-        ~VectorAddition() // nem tom hogy ez itt jo lesz-e
+        ~VectorAddition()
         {
-			// Free OpenCL resources
-			clReleaseKernel(kernel);
-			clReleaseProgram(program);
-			clReleaseCommandQueue(cmdQueue);
-			clReleaseMemObject(bufA);
-			clReleaseMemObject(bufB);
-			clReleaseMemObject(bufC);
-			clReleaseContext(context);
+		// Free OpenCL resources
+		clReleaseKernel(kernel);
+		clReleaseProgram(program);
+		clReleaseCommandQueue(cmdQueue);
+		clReleaseMemObject(bufA);
+		clReleaseMemObject(bufB);
+		clReleaseMemObject(bufC);
+		clReleaseContext(context);
 
-			// Free host resources
-			free(A);
-			free(B);
-			free(C);
-			free(platforms);
-			free(devices);
-		}
+		// Free host resources
+		free(A);
+		free(B);
+		free(C);
+		free(platforms);
+		free(devices);
+	}
         
         int setupVectorAddition();
         
         int setupCL(); // Set up context, Devices, CmdQue and MemBuffs
         void runCLKernels(); // Set kernel args, enque them in CmdQue, wait, start etc...
         void printDeviceInfo();
-		void printPlatformInfo();
-		void checkError(); 
+	void printPlatformInfo();
+	void checkError(); 
     
 };
