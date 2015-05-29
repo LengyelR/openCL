2daaa4abce5bb4a59a01452f9c8a8bb7e0d337fe
LengyelR <richard.lengyel@yahoo.com>
2015-05-29 18:31:13 +0200

    Update vector_addition.cpp

--- vector_addition.cpp
+++ vector_addition.cpp
@@ -1,5 +1,7 @@
-#include "vector_addition.h"
+#include "Vector.h"
 
+#define CARD 1
+#define MAX_SOURCE_SIZE (0x100000)
 
 int VectorAddition::setupCL()
 {
@@ -9,17 +11,17 @@ int VectorAddition::setupCL()
 	platforms = (cl_platform_id*)malloc(numPlatforms*sizeof(cl_platform_id));
 	clGetPlatformIDs(numPlatforms, platforms, NULL);
 
-	clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
+	clGetDeviceIDs(platforms[CARD], CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
 
-	devices=(cl_device_id*)malloc(numDevices*sizeof(cl_device_id));
-	clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_ALL, numDevices, devices, NULL);
+	devices = (cl_device_id*)malloc(numDevices*sizeof(cl_device_id));
+	clGetDeviceIDs(platforms[CARD], CL_DEVICE_TYPE_ALL, numDevices, devices, NULL);
 
-	context=clCreateContext(NULL, numDevices, devices, NULL, NULL, &status);
-	cmdQueue=clCreateCommandQueue(context, devices[0], 0, &status);
+	context = clCreateContext(NULL, numDevices, devices, NULL, NULL, &status);
+	cmdQueue = clCreateCommandQueue(context, devices[CARD], 0, &status);
 
-	bufA=clCreateBuffer(context, CL_MEM_READ_ONLY, datasize, NULL, &status);
-	bufB=clCreateBuffer(context, CL_MEM_READ_ONLY, datasize, NULL, &status);
-    	bufC=clCreateBuffer(context, CL_MEM_WRITE_ONLY, datasize, NULL, &status);
+	bufA = clCreateBuffer(context, CL_MEM_READ_ONLY, datasize, NULL, &status);
+	bufB = clCreateBuffer(context, CL_MEM_READ_ONLY, datasize, NULL, &status);
+	bufC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, datasize, NULL, &status);
 
 	clEnqueueWriteBuffer(cmdQueue, bufA, CL_FALSE, 0, datasize, A, 0, NULL, NULL);
 	clEnqueueWriteBuffer(cmdQueue, bufB, CL_FALSE, 0, datasize, B, 0, NULL, NULL);
@@ -29,104 +31,109 @@ int VectorAddition::setupCL()
 
 void VectorAddition::runCLKernels()
 {
-	const char* programSource =
-	"__kernel                                             \n"
-	"void vectoradd(__global int *A,                  \n"
-	"                 __global int *B,                  \n"
-	"                 __global int *C)                  \n"
-	"{                                                       \n"
-	"                                                         \n"
-	" int idx=get_global_id(0);                     \n"
-	"                                                         \n"
-	"C[idx]=A[idx]+B[idx];                           \n"
-	"}                                                       \n"
-	;
-	program = clCreateProgramWithSource(context, 1, (const char**)&programSource, NULL, &status);      
-	clBuildProgram(program, numDevices, devices,  NULL, NULL, NULL);
-		        
-	kernel=clCreateKernel(program, "vectoradd", &status);
+	FILE *fp;
+	errno_t err;
+	char *source_str;
+	size_t source_size;
+
+	err = fopen_s(&fp, "kernel.cl", "r");
+	if (err) {
+		fprintf(stderr, "HIBA: Failed to open file\n");
+		exit(1);
+	}
+	source_str = (char*)malloc(MAX_SOURCE_SIZE);
+	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+	fclose(fp);
+
+	// Create a program with source code
+	program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &status);
+	// Build (compile) the program for the device
+	clBuildProgram(program, numDevices, devices, NULL, NULL, NULL);
+
+	kernel = clCreateKernel(program, "vectoradd", &status);
+
 	clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufA);
 	clSetKernelArg(kernel, 1, sizeof(cl_mem), &bufB);
 	clSetKernelArg(kernel, 2, sizeof(cl_mem), &bufC);
 
 	size_t globalWorkSize[1];
-	globalWorkSize[0]=elements;
+	globalWorkSize[0] = elements;
 
 	clEnqueueNDRangeKernel(cmdQueue, kernel, 1, //1-dimension
 		NULL, globalWorkSize, NULL, 0, NULL, NULL); //local sizes, wait lists, etc...
 
 	clEnqueueReadBuffer(cmdQueue, bufC, CL_TRUE, 0, datasize, C, 0, NULL, NULL);
 }
-
 void VectorAddition::printPlatformInfo()
 {
-	clGetPlatformInfo(platforms[0], CL_PLATFORM_PROFILE, NULL, profile, &size); 
-	
+	clGetPlatformInfo(platforms[0], CL_PLATFORM_PROFILE, NULL, profile, &size);
+
 	profile = (char*)malloc(size);
-	clGetPlatformInfo(platforms[0], CL_PLATFORM_PROFILE,size, profile, NULL); 
+	clGetPlatformInfo(platforms[0], CL_PLATFORM_PROFILE, size, profile, NULL);
 
 	clGetPlatformInfo(platforms[0], CL_PLATFORM_VERSION, NULL, platform_version, &size);
 
 	platform_version = (char*)malloc(size);
-	clGetPlatformInfo(platforms[0], CL_PLATFORM_VERSION,size, platform_version, NULL); 
+	clGetPlatformInfo(platforms[0], CL_PLATFORM_VERSION, size, platform_version, NULL);
 
 	printf("Platform Informations:\n");
 	printf("(Status now: %d)\n", status);
-	printf("Profle: %s\n",profile);
-	printf("Platform Version: %s\n",platform_version);
+	printf("Profle: %s\n", profile);
+	printf("Platform Version: %s\n", platform_version);
 
 }
 void VectorAddition::printDeviceInfo()
 {
 
-    clGetDeviceInfo(devices[0], CL_DEVICE_VENDOR, NULL, NULL, &size);
-    vendor = (char*)malloc(sizeof(char)*size);
-    clGetDeviceInfo(devices[0], CL_DEVICE_VENDOR, size, vendor, NULL);
+	clGetDeviceInfo(devices[0], CL_DEVICE_VENDOR, NULL, NULL, &size);
+	vendor = (char*)malloc(sizeof(char)*size);
+	clGetDeviceInfo(devices[0], CL_DEVICE_VENDOR, size, vendor, NULL);
 
 	clGetDeviceInfo(devices[0], CL_DEVICE_NAME, NULL, NULL, &size);
-    device_name = (char*)malloc(sizeof(char)*size);
-    clGetDeviceInfo(devices[0], CL_DEVICE_NAME, size, device_name, NULL);
+	device_name = (char*)malloc(sizeof(char)*size);
+	clGetDeviceInfo(devices[0], CL_DEVICE_NAME, size, device_name, NULL);
 
 	clGetDeviceInfo(devices[0], CL_DEVICE_OPENCL_C_VERSION, NULL, NULL, &size);
-    open_cl_c_version = (char*)malloc(sizeof(char)*size);
-    clGetDeviceInfo(devices[0], CL_DEVICE_OPENCL_C_VERSION, size, open_cl_c_version, NULL);
+	open_cl_c_version = (char*)malloc(sizeof(char)*size);
+	clGetDeviceInfo(devices[0], CL_DEVICE_OPENCL_C_VERSION, size, open_cl_c_version, NULL);
 
 	clGetDeviceInfo(devices[0], CL_DEVICE_VERSION, NULL, NULL, &size);
-    open_cl_version = (char*)malloc(sizeof(char)*size);
-    clGetDeviceInfo(devices[0], CL_DEVICE_VERSION, size, open_cl_version, NULL);
+	open_cl_version = (char*)malloc(sizeof(char)*size);
+	clGetDeviceInfo(devices[0], CL_DEVICE_VERSION, size, open_cl_version, NULL);
 
 	clGetDeviceInfo(devices[0], CL_DEVICE_MAX_WORK_GROUP_SIZE, NULL, NULL, &size);
-    max_workgroup = (size_t*)malloc(sizeof(size_t)*size);
-    clGetDeviceInfo(devices[0], CL_DEVICE_MAX_WORK_GROUP_SIZE, size, &max_workgroup, NULL);
+	max_workgroup = (size_t*)malloc(sizeof(size_t)*size);
+	clGetDeviceInfo(devices[0], CL_DEVICE_MAX_WORK_GROUP_SIZE, size, &max_workgroup, NULL);
 
 	clGetDeviceInfo(devices[0], CL_DEVICE_MAX_COMPUTE_UNITS, NULL, NULL, &size);
-    max_cu = (cl_uint*)malloc(sizeof(cl_uint)*size);
-    clGetDeviceInfo(devices[0], CL_DEVICE_MAX_COMPUTE_UNITS, size, &max_cu, NULL);
+	max_cu = (cl_uint*)malloc(sizeof(cl_uint)*size);
+	clGetDeviceInfo(devices[0], CL_DEVICE_MAX_COMPUTE_UNITS, size, &max_cu, NULL);
 
 	printf("\n\nDevice Informations:\n");
 	printf("Vendor: %s\n", vendor);
-	printf("Device: %s\n",device_name);
-	printf("openCL C: %s\n",open_cl_c_version);
-	printf("openCL: %s\n",open_cl_version);
-	printf("Max WorkGroup: %d\n",max_workgroup);
-	printf("Max ComputeUnits: : %d\n\n",max_cu);
+	printf("Device: %s\n", device_name);
+	printf("openCL C: %s\n", open_cl_c_version);
+	printf("openCL: %s\n", open_cl_version);
+	printf("Max WorkGroup: %d\n", max_workgroup);
+	printf("Max ComputeUnits: : %d\n\n", max_cu);
 }
 
 void VectorAddition::checkError()
 {
-    int result=1;
-    for(int i=0; i<elements; i++) {
-        if(C[i] != i+i) {
-            result=0;
-            break;
-        }
-    }
+	int result = 1;
+	for (int i = 0; i<elements; i++) {
+		if (C[i] != i + i) {
+			result = 0;
+			break;
+		}
+	}
 
-    if(result) {
-        printf("Output is correct\n");
-    } else {
-        printf("Output is incorrect\n");
-    }
+	if (result) {
+		printf("Output is correct\n");
+	}
+	else {
+		printf("Output is incorrect\n");
+	}
 }
 
 int main()
