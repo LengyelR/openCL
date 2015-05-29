d6c644410eb59ea1dc0270cef59a944bb97276c7
LengyelR <richard.lengyel@yahoo.com>
2015-05-30 00:12:12 +0200

    Read from file

--- vector_addition.h
+++ vector_addition.h
@@ -1,3 +1,4 @@
+#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 #include <CL/cl.h>
 #include<stdio.h>
 #include<stdlib.h>
@@ -6,32 +7,32 @@
 
 class VectorAddition
 {
-    protected:
-    
-        const int elements;
-    
-        size_t datasize;
-        size_t size;
+protected:
 
-        // Host buffers
-        int *A;
-        int *B;
-        int *C;
+	const int elements;
 
-        // Device buffers
-        cl_mem bufA;
-        cl_mem bufB;
-        cl_mem bufC;
-        
-        // CL objects
+	size_t datasize;
+	size_t size;
+
+	// Host buffers
+	int *A;
+	int *B;
+	int *C;
+
+	// Device buffers
+	cl_mem bufA;
+	cl_mem bufB;
+	cl_mem bufC;
+
+	// CL objects
 	cl_int status;
-        cl_uint numPlatforms;
-        cl_uint numDevices;
-        cl_platform_id *platforms;
-        cl_device_id *devices;
-        cl_context context;
-        cl_command_queue cmdQueue;
-        cl_program program;
+	cl_uint numPlatforms;
+	cl_uint numDevices;
+	cl_platform_id *platforms;
+	cl_device_id *devices;
+	cl_context context;
+	cl_command_queue cmdQueue;
+	cl_program program;
 	cl_kernel kernel;
 
 	//Info Objects(platform)
@@ -41,16 +42,16 @@ class VectorAddition
 	//Info Objects(device)
 	char *vendor;				// CL_DEVICE_VENDOR
 	char *device_name;			// CL_DEVICE_NAME
-	char *open_cl_c_version;		// CL_DEVICE_OPENCL_C_VERSION
-	char *open_cl_version;			// CL_DEVICE_VERSION
+	char *open_cl_c_version;		// CL_DEVICE_OPENCL_C_VERSION  ---- openCL C
+	char *open_cl_version;			// CL_DEVICE_VERSION --- openCL
 	size_t *max_workgroup;			// CL_DEVICE_MAX_WORK_GROUP_SIZE
 	cl_uint *max_cu;			// CL_DEVICE_MAX_COMPUTE_UNITS
 	cl_uint *max_freq;			// CL_DEVICE_MAX_CLOCK_FREQUENCY
-        
-    public:
-        VectorAddition()
-              : elements(NUM_OF_ELEMENTS),
-                datasize(sizeof(int)*elements),
+
+public:
+	VectorAddition()
+		: elements(NUM_OF_ELEMENTS),
+		datasize(sizeof(int)*elements),
 		A((int*)malloc(datasize)), //ezekek szintén?
 		B((int*)malloc(datasize)),
 		C((int*)malloc(datasize)),
@@ -71,16 +72,17 @@ class VectorAddition
 		max_workgroup(NULL),
 		max_cu(NULL),
 		max_freq(NULL)
-        {
-		for(int i=0; i<elements; i++) {
-			A[i]=i;
-			B[i]=i;
+
+	{
+		for (int i = 0; i<elements; i++) {
+			A[i] = i;
+			B[i] = i;
 		}
 
-        }
-            
-        ~VectorAddition()
-        {
+	}
+
+	~VectorAddition() // nem tom hogy ez itt jo lesz-e
+	{
 		// Free OpenCL resources
 		clReleaseKernel(kernel);
 		clReleaseProgram(program);
@@ -97,13 +99,13 @@ class VectorAddition
 		free(platforms);
 		free(devices);
 	}
-        
-        int setupVectorAddition();
-        
-        int setupCL(); // Set up context, Devices, CmdQue and MemBuffs
-        void runCLKernels(); // Set kernel args, enque them in CmdQue, wait, start etc...
-        void printDeviceInfo();
+
+	int setupVectorAddition();
+
+	int setupCL(); // Set up context, Devices, CmdQue and MemBuffs
+	void runCLKernels(); // Set kernel args, enque them in CmdQue, wait, start etc...
+	void printDeviceInfo();
 	void printPlatformInfo();
-	void checkError(); 
-    
+	void checkError();
+
 };
